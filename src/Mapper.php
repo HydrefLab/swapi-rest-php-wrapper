fd375216243d331cef895f8825be25a21bce5b20
Michał Golon <michal.golon@gmail.com>
2017-06-22 14:39:49 +0200

    Mapper: add method for parsing annotations - copied from PHPUnit

--- src/Mapper.php
+++ src/Mapper.php
@@ -35,4 +35,28 @@ class Mapper
 
         return $object;
     }
+
+    /**
+     * Method copied from https://github.com/sebastianbergmann/phpunit/blob/master/src/Util/Test.php
+     *
+     * @param string $docBlock
+     * @return array
+     */
+    protected function parseAnnotations(string $docBlock): array
+    {
+        $annotations = [];
+
+        // Strip away the docblock header and footer to ease parsing of one line annotations
+        $docBlock = substr($docBlock, 3, -2);
+
+        if (preg_match_all('/@(?P<name>[A-Za-z_-]+)(?:[ \t]+(?P<value>.*?))?[ \t]*\r?$/m', $docBlock, $matches)) {
+            $numMatches = count($matches[0]);
+
+            for ($i = 0; $i < $numMatches; ++$i) {
+                $annotations[$matches['name'][$i]][] = (string) $matches['value'][$i];
+            }
+        }
+
+        return $annotations;
+    }
 }
